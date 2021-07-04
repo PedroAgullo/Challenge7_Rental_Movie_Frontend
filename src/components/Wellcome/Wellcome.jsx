70dfccc1686b5a4780e34f2d1232a2e74b763db6
Pedro <Pedro.agullo.marco@gmail.com>
2021-07-04 04:36:45 +0200

    Added wellcome component. Added buy premium option.

---
+++ src/components/Wellcome/Wellcome.jsx
@@ -0,0 +1,54 @@
+
+import React from 'react';
+import {connect} from 'react-redux';
+import Premiumfoto from '../../images/Premium.jpg';
+import Logo1 from '../../images/Logo4.jpg';
+import {notification} from 'antd';
+import {useHistory} from "react-router";
+
+const Wellcome = (props) => {
+
+    let history = useHistory();
+
+    let buyPremium = () => {
+        if (props.credentials?.token === '') {
+            notification.warning({message:'Registrate o haz login',description: "Para acceder a las ventajas de ser premium, regístrate o haz login." });        
+
+        }else{
+            history.push('profile');
+        }
+    }
+
+
+
+    return (
+        <div className="SearchDiv">
+            <div className="box">
+                <div>
+                    <img src={Logo1} alt="Logo" className="fotoWellcome"/>
+                </div>
+                <div>
+                    <p>Bienvenidos a "El Séptimo Arte". Tu servicio de video bajo de manda.</p>
+                    <p>Con nosotros podrás acceder a una biblioteca de cientos de películas.</p>
+                    <p>Alquila o compra una película para verla cuantas veces quiera.</p>                
+                </div>
+            </div>
+
+            <div className="box">
+                <div>
+                    <img src={Premiumfoto} id="fotopremium"alt="Premium" className="fotoWellcome" onClick={()=>buyPremium()}/>
+                </div>
+                <div>
+                    <p>Con nuestra subscripción Premium  accederás a todas nuestras películas.</p> 
+                    <p> Con la versión Premium obtiense acceso a una zona infantil. </p>
+                    <p>Dispones de un control parental para limitar el acceso a las películas que tú quieras.</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default connect((state) => ({
+    user: state.credentials.user,
+    tipodatos: state.tipodatos
+}))(Wellcome);
--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import TopRated from '../../components/TopRated/TopRated';
 import Popular from '../../components/Popular/Popular';
 import Novedades from '../../components/Novedades/Novedades';
+import Wellcome from '../../components/Wellcome/Wellcome';
 
 
 const Home = () => {
 
     return (
         <div className="HomeDiv">
-
-               <TopRated/>
-               <Popular/>
-               <Novedades/>
-
+                <TopRated/>
+                <Popular/>
+                <Novedades/>
+                <Wellcome/>
         </div>
     );
 }
